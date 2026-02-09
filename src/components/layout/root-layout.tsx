3902a58510f5c181c9c9dff5c30b58c19c6874de
Elwqnn <elwann.guillemot@epitech.eu>
2026-02-09 22:15:03 +0100

    fix: move background to static HTML to prevent flash on reload

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
 import { SiteHeader } from "./site-header";
 import { SiteFooter } from "./site-footer";
-import { BackgroundEffects } from "@/components/ui/background-effects";
 
 export function RootLayout() {
   return (
     <>
-      <BackgroundEffects />
       <div className="relative z-10 flex min-h-svh flex-col">
         <SiteHeader />
         <main className="mx-auto w-full max-w-3xl flex-1 px-6 py-10">
